7a0fcfd94858dfa3cb17be2de1976f8e3e1c2fc5
Nikita <vip.nikita.2004@list.ru>
2020-11-08 20:10:57 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,99 @@
+let $start = document.querySelector('#start');
+let $game = document.querySelector('#game');
+let $time = document.querySelector('#time');
+let $result = document.querySelector('#result');
+let $timeHeader = document.querySelector('#time-header');
+let $resultHeader = document.querySelector('#result-header');
+let $gameTime = document.querySelector('#game-time');
+
+let colors = ['red', 'blue', 'yellow', 'green', 'pink']
+let score = 0;
+let isGameStarted = false;
+
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
+$gameTime.addEventListener('input', setGameTime)
+
+function show($el){
+    $el.classList.remove('hide');
+}
+
+function hide($el){
+    $el.classList.add('hide');
+}
+
+function startGame(){
+    score = 0;
+    setGameTime();
+    $gameTime.setAttribute('disabled', 'true');
+    hide($start);
+    $game.style.background = '#fff';
+
+    let interval = setInterval(function(){
+        isGameStarted = true;
+        let time = parseFloat($time.textContent);
+        if (time <= 0){
+            clearInterval(interval);
+            endGame();
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1);
+        }
+    }, 100);
+
+    renderBox();
+}
+
+function setGameScore(){
+    $result.textContent = score.toString();
+}
+
+function setGameTime(){
+    let time = +$gameTime.value;
+    $time.textContent = time.toFixed(1);
+    show($timeHeader);
+    hide($resultHeader);
+}
+
+function endGame(){
+    isGameStarted = false;
+    setGameScore();
+    $gameTime.removeAttribute('disabled');
+    show($start)
+    $game.innerHTML = '';
+    $game.style.backgroundColor = '#ccc';
+    hide($timeHeader);
+    show($resultHeader);
+}
+
+function handleBoxClick(event){
+    if(!isGameStarted){
+        return false
+    }
+    if(event.target.dataset.box){
+        score++
+        renderBox();
+    }
+}
+
+function renderBox(){
+    $game.innerHTML = '';
+    let box = document.createElement('div');
+    let boxSize = getRandom(20,100);
+    let gameSize = $game.getBoundingClientRect();
+    let maxTop = gameSize.height - boxSize;
+    let maxLeft = gameSize.width - boxSize;
+
+    box.style.height = box.style.width = boxSize + 'px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = colors[getRandom(0, colors.length)];
+    box.style.top = getRandom(0, maxTop) + 'px';
+    box.style.left = getRandom(0, maxLeft) + 'px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', 'true')
+
+    $game.insertAdjacentElement("afterbegin", box)
+}
+
+function getRandom(min, max){
+    return Math.floor(Math.random() * (max - min) + min)
+}
